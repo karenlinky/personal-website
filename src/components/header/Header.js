18143c08e03c8c56ccafe1ad02bb8556daf2b975
Karen Lin <karenlinky@gmail.com>
2023-04-15 19:34:54 -0400

    created narrow menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import MenuBar from './MenuBar';
+import MenuList from './MenuList';
 import './header.css';
 
 const Header = () => {
   return (
-    <div className={'headerBar'}>
-        <MenuBar />
-    </div>
+    <>
+      <div className={'headerBar wideHeaderBar'}>
+          <MenuBar />
+      </div>
+      <div className={'headerBar narrowHeaderBar'}>
+          <MenuList />
+      </div>
+    </>
   )
 }
 
