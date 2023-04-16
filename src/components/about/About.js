30e470af3d4df564d19e680a98726da6c03b388a
Karen Lin <karenlinky@gmail.com>
2023-04-15 20:23:23 -0400

    Added name to homepage

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Header from '../header/Header';
+import '../content/content.css';
 import './about.css';
 
 const About = () => {
   return (
     <>
         <Header />
-        <div className="aboutContent">About</div>
+        <div className={'fullPageBody'}>
+          <div className={'aboutContent'}>
+            <div className={'name nameLine1'}>
+              KA YING
+            </div>
+            <div className={'name nameLine2'}>
+              KAREN LIN
+            </div>
+          </div>
+        </div>
+
     </>
   )
 }
--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import About from './about/About';
+import About from '../about/About';
 import './content.css'
 
 const Content = () => {
