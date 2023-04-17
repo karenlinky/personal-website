41fa9e4460e75d33d9ae11ea629fb5ad787c89f4
Karen Lin <karenlinky@gmail.com>
2023-04-17 18:07:25 -0400

    added expandable content to card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './card.css';
 
-const Card = ({ children, className }) => {
+const Card = ({ children, className, expandableContent }) => {
+
+  var additionalClassName = "";
+  if (expandableContent !== null) {
+    additionalClassName = " expandable"
+  }
+
   return (
-    <div className={'card' + (className ? ' ' + className : '')}>{children}</div>
+    <div className={'card' + additionalClassName + (className ? ' ' + className : '')}>{children}</div>
   )
 }
 
--- src/components/workExperiences/WorkExperiences.js
+++ src/components/workExperiences/WorkExperiences.js
@@ -7,16 +7,24 @@ import CardSubheader from '../card/CardSubheader';
 import CardContent from '../card/CardContent';
 
 const WorkExperiences = () => {
+  const MetaDescription = <div>
+    <div>Used ReactJS and Hack to build new features</div>
+    <div>Built an alert inbox page using a new React component library</div>
+    <div>Added a test-launch feature to the developer communication platform</div>
+    <div>Added validations to restrict recipient type of a test-launch</div>
+    <div>Added logic for message’s status check</div>
+    <div>Designed the UI using Figma</div>
+    <div>Added alert-template feature that allows users to create new alerts using templates</div>
+  </div>;
+
   return (
     <ResponsivePage>
         <PageHeader label={'Work Experiences'}/>
-        <Card className={'workExperiencesCard'}>
+        <Card className={'workExperiencesCard'} expandableContent={MetaDescription}>
           <CardContent  className={'workExperiencesContent'}>
             <CardHeader>Meta, SWE Intern</CardHeader>
             <CardSubheader>Seattle</CardSubheader>
             <CardSubheader>2022 May - 2022 Aug</CardSubheader>
-
-            
           </CardContent>
         </Card>
     </ResponsivePage>
