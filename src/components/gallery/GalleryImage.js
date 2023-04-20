858069ead3be2564d326d86090230f3408667bd0
Karen Lin <karenlinky@gmail.com>
2023-04-20 18:33:44 -0400

    made gallery image fade in

--- src/components/gallery/GalleryImage.js
+++ src/components/gallery/GalleryImage.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import './gallery.css';
 
 const GalleryImage = ({ children }) => {
-  return (
-    <div className={'galleryImage'}>{children}</div>
-  )
+
+    const { ref, inView } = useInView({threshold: 0.2})
+    // useEffect(() => {
+    //     onInViewChange(inView);
+    // }, [inView, onInViewChange]);
+
+    // return (
+    //     <div ref={ref}>{children}</div>
+    // )
+
+    // const [inView, setInView] = useState(false);
+
+    return (
+        <div ref={ref} className={'galleryImage' + (inView ? ' showGalleryImage' : ' hideGalleryImage')}>
+            {children}
+        </div>
+    )
 }
 
 export default GalleryImage
