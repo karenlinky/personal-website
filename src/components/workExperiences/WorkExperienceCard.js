e1aaeaedb7967e44fe487b362c20ae699fef73bd
Karen Lin <karenlinky@gmail.com>
2023-04-19 20:14:44 -0400

    Animate work experiences when scrolled to

--- src/components/workExperiences/WorkExperienceCard.js
+++ src/components/workExperiences/WorkExperienceCard.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 import Card from '../card/Card';
 import CardHeader from '../card/CardHeader';
 import CardSubheader from '../card/CardSubheader';
 
 const WorkExperienceCard = ({ company, position, location, dateRange, description, openNum, experienceNum, onCardClick }) => {
+
+  const { ref, inView } = useInView({threshold: 0.5})
+
   return (
-    <Card
-        className={'workExperiencesCard'}
-        cardContentClassName={'workExperiencesContent'}
-        expandableContent={description}
-        openNum={openNum}
-        experienceNum={experienceNum}
-        onCardClick={onCardClick}>
-        <CardHeader>{company}, {position}</CardHeader>
-        <CardSubheader>{location}</CardSubheader>
-        <CardSubheader>{dateRange}</CardSubheader>
-    </Card>
+    <div ref={ref}>
+      <Card
+          className={'workExperiencesCard' + (inView ? '' : ' hideCard')}
+          cardContentClassName={'workExperiencesContent'}
+          expandableContent={description}
+          openNum={openNum}
+          experienceNum={experienceNum}
+          onCardClick={onCardClick}>
+          <CardHeader>{company}, {position}</CardHeader>
+          <CardSubheader>{location}</CardSubheader>
+          <CardSubheader>{dateRange}</CardSubheader>
+      </Card>
+    </div>
   )
 }
 
