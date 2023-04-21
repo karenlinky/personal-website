ea0f0ce5ffceb29b780d35bf869b5e7bc6624860
Karen Lin <karenlinky@gmail.com>
2023-04-20 23:20:39 -0400

    Added icons to narrow menu

--- src/components/header/MenuList.js
+++ src/components/header/MenuList.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react'
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import MenuListItem from './MenuListItem';
+import IconButtonGroup from '../iconButton/IconButtonGroup';
 import './header.css';
 
 const MenuList = () => {
@@ -16,7 +17,7 @@ const MenuList = () => {
 
   return (
     <div className={'narrowMenuContainer' + (showMenuList ? ' showNarrowMenuContainer' :  ' hideNarrowMenuContainer')}>
-        <FaBars onClick={toggleMenu} className={'menuIcon'}/>
+        {showMenuList ? <FaTimes onClick={toggleMenu} className={'menuIcon'} /> : <FaBars onClick={toggleMenu} className={'menuIcon'}/>}
         <div className={'narrowMenuScrollable'}>
           <div className={'narrowMenuList' + (showMenuList ? ' showNarrowMenuList' :  ' hideNarrowMenuList')}>
             <MenuListItem closeMenu={closeMenu} label={'Home'} to={'/home'}/>
@@ -24,6 +25,9 @@ const MenuList = () => {
             <MenuListItem closeMenu={closeMenu} label={'Projects'} to={'projects'}/>
             <MenuListItem closeMenu={closeMenu} label={'UI Design'} to={'ui_design'}/>
             <MenuListItem closeMenu={closeMenu} label={'3D Modeling'} to={'3d_modeling'}/>
+            <div className={'narrowMenuIconGroup'}>
+              <IconButtonGroup />
+            </div>
           </div>
         </div>
     </div>
--- src/components/header/MenuListItem.js
+++ src/components/header/MenuListItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import MenuItemText from './MenuItemText';
 import './header.css';
 
 const MenuListItem = ({ label, to, closeMenu }) => {
   return (
-    <div onClick={closeMenu}><NavLink className={'menuListItem'} to={to} >{ label }</NavLink></div>
+    <div onClick={closeMenu}><NavLink className={'menuListItem'} to={to} ><MenuItemText label={label} /></NavLink></div>
   )
 }
 
