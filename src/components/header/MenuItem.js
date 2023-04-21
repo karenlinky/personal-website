14e391d4481af2faf4dfe57609bf5d4feb016ca4
Karen Lin <karenlinky@gmail.com>
2023-04-20 21:57:51 -0400

    Removed comments

--- src/components/header/MenuItem.js
+++ src/components/header/MenuItem.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import MenuItemText from './MenuItemText';
-import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import './header.css';
 
 const MenuItem = ({label, to}) => {
-  
-
-// {/* <NavLink to='about' activeClassName="active">about</NavLink> */}
   return (
     <NavLink className={'menuItem'} to={to}>
       <MenuItemText label={label} />
