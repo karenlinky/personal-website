85839434dba578db27fa08892ce89e7432cf5933
Karen Lin <karenlinky@gmail.com>
2023-04-22 21:47:15 -0400

    Adjusted projectSection paddings

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -7,9 +7,10 @@ import profilePic from '../../img/profilePic/profilePic.jpg';
 
 const AboutMe = () => {
     const descr = <><div>I'm a software engineer. I'm studying Computer Science at University of Waterloo and I'm graduating in 2023 April.
-        Through my internships and projects, I've gained experiences on both front-end and back-end developing.
-        Full-stack development is particularly appealing to me due to the level of ownership it provides.
-        Moreover, I also enjoy developing the frontend (including designing the UI) because I like making visually impactful work.
+        I have gained experience in both front-end and back-end development through my internships and projects.
+        I find full-stack development particularly interesting because of the sense of ownership it provides.
+        Moreover, I also enjoy working on the front-end, including UI design, because I like making visually impactful work.<br/>
+        I have a lot of hobbies. Other than coding, I like playing table tennis, basketball, badminton, ultimate frisbee and soccer, practicing wing chun, swimming, and doing 3D modeling.
         </div></>
     
     return (
