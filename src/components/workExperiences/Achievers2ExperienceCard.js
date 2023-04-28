ce5a4d9188813a08e7ab7c3edcd912fad5c6bc20
Karen Lin <karenlinky@gmail.com>
2023-04-27 21:52:18 -0400

    fixed typo

--- src/components/workExperiences/Achievers2ExperienceCard.js
+++ src/components/workExperiences/Achievers2ExperienceCard.js
@@ -15,10 +15,10 @@ const Achievers2ExperienceCard = ({ openNum, experienceNum, onCardClick }) => {
     <div><Chip>ReactJS</Chip><Chip>SCSS</Chip><Chip>Python</Chip><Chip>OOP</Chip></div>
     <ListItem1>Added <CardPrimaryText>user-input validation</CardPrimaryText> for the announcement edit form</ListItem1>
     <ListItem2>Used <CardPrimaryText>Formik</CardPrimaryText> and <CardPrimaryText>Yup</CardPrimaryText> to validate user-input on the frontend</ListItem2>
-    <ListItem2>Wrote a class to <CardPrimaryText>validate request data</CardPrimaryText> received on the backend</ListItem2>
-    <ListItem1>Added styling to images/GIFs of regognition post edit form</ListItem1>
+    <ListItem2>Wrote a class to <CardPrimaryText>validate data</CardPrimaryText> received on the backend</ListItem2>
+    <ListItem1>Added styling to images/GIFs of recognition post edit form</ListItem1>
     <ListItem2>Created a <CardPrimaryText>blurred</CardPrimaryText> copy of an image, <CardPrimaryText>proportionally enlarged</CardPrimaryText> it, and used it as a background</ListItem2>
-    <ListItem2>Put the original image on top of the blurried image</ListItem2>
+    <ListItem2>Put the original image on top of the blurred image</ListItem2>
     <ImageExample />
     <ListItem1>Fixed <CardPrimaryText>frontend</CardPrimaryText> and <CardPrimaryText>backend</CardPrimaryText> bugs</ListItem1>
     <ListItem2>Made the UI more <CardPrimaryText>responsive</CardPrimaryText> by removing unwanted white spaces according to the screensize</ListItem2>
