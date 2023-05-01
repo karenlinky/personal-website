1aaf80b7a23016f63db526cdab1677a78b78fbed
Karen Lin <karenlinky@gmail.com>
2023-05-01 15:43:30 -0400

    updated about me description

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -8,7 +8,7 @@ import './aboutMe.css';
 import profilePic from '../../img/profilePic/profilePic.jpg';
 
 const AboutMe = () => {
-    const descr = <><div>I'm a software engineer. I'm studying Computer Science at University of Waterloo and I'm graduating in 2023 April.
+    const descr = <><div>I'm a software engineer. I graduated from the University of Waterloo with a Bachelor's degree in Computer Science.
         I have gained experience in both front-end and back-end development through my internships and projects.
         I find full-stack development particularly interesting because of the sense of ownership it provides.
         Moreover, since I like making visually impactful work, I also enjoy working on the front-end, including UI design.<br/>
