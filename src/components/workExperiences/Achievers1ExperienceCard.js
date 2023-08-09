de348ec97676a562ad13922af1ba61be317f2799
Karen Lin <karenlinky@gmail.com>
2023-08-09 18:37:44 -0400

    updated experience

--- src/components/workExperiences/Achievers1ExperienceCard.js
+++ src/components/workExperiences/Achievers1ExperienceCard.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import WorkExperienceCard from './WorkExperienceCard';
-import ListItem1 from '../listItems/ListItem1';
-import ListItem2 from '../listItems/ListItem2';
-import Chip from '../chip/Chip';
-import CardPrimaryText from '../card/CardPrimaryText';
+import React from "react";
+import WorkExperienceCard from "./WorkExperienceCard";
+import ListItem1 from "../listItems/ListItem1";
+import ListItem2 from "../listItems/ListItem2";
+import Chip from "../chip/Chip";
+import CardPrimaryText from "../card/CardPrimaryText";
 
 const Achievers1ExperienceCard = ({ openNum, experienceNum, onCardClick }) => {
-  const company = 'Achievers';
-  const position = 'SWE Intern';
-  const location = 'Toronto, ON, CA';
-  const dateRange = '2021 Jan - 2021 Apr';
-  const description = <div>
-    <div><Chip>PHP</Chip><Chip>Unit Test</Chip></div>
-    <ListItem1>Unit test</ListItem1>
-    <ListItem2>Performed <CardPrimaryText>unit testing</CardPrimaryText></ListItem2>
-    <ListItem2>Used <CardPrimaryText>PHPUnit</CardPrimaryText> to mock method calls and data</ListItem2>
-    <ListItem1>Allowed the application to <CardPrimaryText>switch between database port</CardPrimaryText> according to the SQL query being executed</ListItem1>
-  </div>;
-    
+  const company = "Achievers";
+  const position = "SWE Intern";
+  const location = "Toronto, ON, CA";
+  const dateRange = "2021 Jan - 2021 Apr";
+  const description = (
+    <div>
+      <div>
+        <Chip>PHP</Chip>
+        <Chip>Unit Test</Chip>
+      </div>
+      <ListItem1>Unit test</ListItem1>
+      <ListItem2>
+        Performed <CardPrimaryText>unit testing</CardPrimaryText>
+      </ListItem2>
+      <ListItem2>
+        Used <CardPrimaryText>PHPUnit</CardPrimaryText> to mock method calls and
+        data
+      </ListItem2>
+    </div>
+  );
+
   return (
     <WorkExperienceCard
       company={company}
@@ -27,8 +36,9 @@ const Achievers1ExperienceCard = ({ openNum, experienceNum, onCardClick }) => {
       description={description}
       openNum={openNum}
       experienceNum={experienceNum}
-      onCardClick={onCardClick} />
-  )
-}
+      onCardClick={onCardClick}
+    />
+  );
+};
 
-export default Achievers1ExperienceCard
+export default Achievers1ExperienceCard;
