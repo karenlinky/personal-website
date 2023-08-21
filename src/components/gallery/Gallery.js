335ed85f3f777e34f3062cffce06cd77f30ae18e
Karen Lin <karenlinky@gmail.com>
2023-08-21 19:40:45 -0400

    Made gallery scrollable

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import { useInView } from 'react-intersection-observer';
-import './gallery.css';
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import { useHorizontalScroll } from "../horizontalScroll/horizontalScroll";
+import "./gallery.css";
 
 const Gallery = ({ children }) => {
-
-  const { ref, inView } = useInView({threshold: 0.1});
+  const { ref, inView } = useInView({ threshold: 0.1 });
+  const scrollRef = useHorizontalScroll();
 
   return (
-    <div ref={ref} className={'galleryContainer' + (inView === false ? ' hideGalleryContainer' : ' showGalleryContainer')}>
-      <div className={'gallery'}>{children}</div>
+    <div
+      ref={scrollRef}
+      className={
+        "galleryContainer showGalleryContainer" // +
+        // (inView === false ? " hideGalleryContainer" : " showGalleryContainer")
+      }
+    >
+      <div ref={scrollRef} className={"galleryScroll"}>
+        <div className={"gallery"}>{children}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
